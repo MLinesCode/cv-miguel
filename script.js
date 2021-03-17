64a255e2e0f3b96b9acd91b9399e859acd593f3c
Specter <mikexx1264@gmail.com>
2021-03-17 14:37:34 -0600

    minify js / ui

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
